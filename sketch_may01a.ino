daf6f91d8282b563ba5714b89ebec513596a1e35
AdrianHsu <adrianhsu1995@gmail.com>
2016-05-01 17:21:24 +0800

    done

--- sketch_may01a.ino
+++ sketch_may01a.ino
@@ -16,39 +16,48 @@ void setup() {
   digitalWrite(TRIGGER_PIN, LOW);
   digitalWrite(MOTOR_PIN, LOW);
   digitalWrite(BUZZER_PIN, LOW);
+  delay(5000);
 }
 
 void loop() {
 
   if(!OMG_GONNA_HIT) {
     digitalWrite(TRIGGER_PIN, HIGH);
-    digitalWrite(MOTOR_PIN, HIGH);
     delayMicroseconds(10);
     digitalWrite(TRIGGER_PIN, LOW);
 
+    digitalWrite(MOTOR_PIN, HIGH);
+    
     long duration = pulseIn(ECHO_PIN, HIGH);
     Serial.print("duration = ");
     Serial.println(duration);
-    long distance = duration * 0.000001 * (34000 / 2);
+    long distance = 1.0 * duration * 0.000001 * (34000 / 2);
     Serial.print("distance = ");
     Serial.println(distance);
 
     if(duration == 0)
       return;
-    
-    if(distance < 5) {
+
+    if(distance < 10) {
       OMG_GONNA_HIT = 1;
       digitalWrite(MOTOR_PIN, LOW);
-      
       digitalWrite(BUZZER_PIN, HIGH);
       Serial.println("BUZZER start");
-      delay(1000);
+      delay(2000);
       Serial.println("BUZZER stop");
       digitalWrite(BUZZER_PIN, LOW);
-
     }
-  } else {
-
-    Serial.println("OMG! stopped.");
-  }
+    else if(distance < 150) {
+      delay(600);
+      digitalWrite(MOTOR_PIN, LOW);
+      digitalWrite(BUZZER_PIN, HIGH);
+      Serial.println("BUZZER start");
+      delay(500);
+      Serial.println("BUZZER stop");
+      digitalWrite(BUZZER_PIN, LOW);
+    }
+    else {
+      delay(100);
+    }
+  } 
 }
